44b9f17813fd174c29c2ff301e52412ae6fbd0ab
Catherine <kazimek9@gmail.com>
2022-12-20 23:26:56 +0300

    исправление по п 1 и 2

--- ps_aux_parse.py
+++ ps_aux_parse.py
@@ -3,7 +3,7 @@ from collections import Counter
 import re
 from datetime import datetime
 
-LINE_REGEX = r'^([\w\.]+)\s+\S+\s+(\d+[,\.]\d+)\s+(\d+[,\.]\d+)\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s(.+)$'
+LINE_REGEX = r'^(\S+)\s+\S+\s+(\d+[,\.]\d+)\s+(\d+[,\.]\d+)\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s(.+)$'
 
 
 def line_to_dict(line):
@@ -50,14 +50,21 @@ parsed_lines.sort(key=lambda v: v['cpu'])
 most_expensive_by_cpu = parsed_lines[-1]['cmd'][:20]
 current_date = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
+
+def print_output(text, file):
+    print(text, file=file)
+    print(text)
+
+
 with open(current_date + '-scan' + '.txt', 'w') as file:
-    print('Отчёт о состоянии системы:', file=file)
-    print('Пользователи системы: {}'.format(system_users), file=file)
-    print('Процессов запущено: {}'.format(len(list(parsed_lines))), file=file)
-    print('Пользовательских процессов:', file=file)
+    print_output('Отчёт о состоянии системы:', file)
+    print_output('Пользователи системы: {}'.format(system_users), file)
+    print_output('Процессов запущено: {}'.format(len(list(parsed_lines))), file)
+    print_output('Пользовательских процессов:', file)
     for user_name in user_counter.keys():
-        print('{}: {}'.format(user_name, user_counter[user_name]), file=file)
-    print('Всего памяти используется: {}%'.format(round(total_memory, 1)), file=file)
-    print('Всего CPU используется: {}%'.format(round(total_cpu, 1)), file=file)
-    print('Больше всего памяти использует: {}'.format(most_expensive_by_memory), file=file)
-    print('Больше всего CPU использует: {}'.format(most_expensive_by_cpu), file=file)
+        print_output('{}: {}'.format(user_name, user_counter[user_name]), file)
+    print_output('Всего памяти используется: {}%'.format(round(total_memory, 1)), file)
+    print_output('Всего CPU используется: {}%'.format(round(total_cpu, 1)), file)
+    print_output('Больше всего памяти использует: {}'.format(most_expensive_by_memory), file)
+    print_output('Больше всего CPU использует: {}'.format(most_expensive_by_cpu), file)
+
